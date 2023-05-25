8a9219dfc3343a2260b7d47c9964a826499b37a5
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-05-25 14:54:25 -0400

    GETTING STARTED

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <v-btn size="x-small">Extra small Button</v-btn>
+      </v-col>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <v-col cols="auto">
+        <v-btn size="small">Small Button</v-btn>
+      </v-col>
 
-  <main>
-    <TheWelcome />
-  </main>
+      <v-col cols="auto">
+        <v-btn>Regular Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn size="large">Large Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn size="x-large">X-Large Button</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
