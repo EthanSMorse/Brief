81dae363767b949fbf5b0db5548d15086c08c979
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-06-05 14:52:21 -0400

    Working on it

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,14 @@
-<script >
+<script setup>
+import {ref} from 'vue'
+
+let input = ref ("")
+let output = ref ("")
+
+async function search () {
+    const response = await fetch ('https://api.dictionaryapi.dev/api/v2/entries/en/' + input.value);
+    output = response
+
+}
 </script>
 
 <template>
@@ -7,7 +17,12 @@
   <router-link to="/search">Search</router-link>
   <router-link to="/contact">Contact Us</router-link>
  </nav>
- <router-view/>
+
+ <br>
+ 
+ <v-text-field label="Search" @keydown.enter="search" v-model="input"></v-text-field>
+ <button @click="search">FETCH</button>
+ {{ output.value }}
 </template>
 
 <style >
